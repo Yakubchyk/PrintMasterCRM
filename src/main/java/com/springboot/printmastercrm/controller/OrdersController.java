9f55ce2bb4b5c5fec5ef8d42148708bd6523beee
Yakubchyk <ivan.yakubchyk@gmail.com>
2025-06-12 11:10:03 +0200

    15.1 New menu

--- src/main/java/com/springboot/printmastercrm/controller/OrdersController.java
+++ src/main/java/com/springboot/printmastercrm/controller/OrdersController.java
@@ -1,57 +1,38 @@
 package com.springboot.printmastercrm.controller;
 
-import com.springboot.printmastercrm.entity.Customer;
-import com.springboot.printmastercrm.entity.PostPress;
-import com.springboot.printmastercrm.entity.Printing;
-import com.springboot.printmastercrm.service.CustomerService;
-import com.springboot.printmastercrm.service.PostPressService;
-import com.springboot.printmastercrm.service.PrintingService;
+import com.springboot.printmastercrm.entity.Order;
+import com.springboot.printmastercrm.entity.OrderStatus;
+import com.springboot.printmastercrm.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/profile")
+@RequestMapping("/orders")
 public class OrdersController {
 
     @Autowired
-    private PostPressService postPressService;
+    private OrderService orderService;
 
-    @Autowired
-    private PrintingService printingService;
-
-    @Autowired
-    private CustomerService customerService;
-
-    @GetMapping("/orders/{id}")
-    public String getOrdersForCustomer(@PathVariable Long id, Model model) {
-
-        Customer customer = customerService.findById(id);
-        if (customer == null) {
-            throw new IllegalArgumentException("Customer not found with ID: " + id);
+    @GetMapping
+    public String viewOrders(@RequestParam(name = "status", required = false) String status, Model model) {
+        List<Order> orders;
+        if (status == null || status.equals("ALL")) {
+            orders = orderService.getAllOrders();
+        } else {
+            orders = orderService.getOrdersByStatus(OrderStatus.valueOf(status));
         }
-
-        // Получить расчеты тиснения и печати для клиента
-        List<PostPress> postPressList = postPressService.findByCustomerId(id);
-        List<Printing> printingList = printingService.findByCustomerId(id);
-
-        // Добавить данные в модель
-        model.addAttribute("selectedCustomer", customer);
-        model.addAttribute("postPressList", postPressList);
-        model.addAttribute("printingList", printingList);
-
-        // Вернуть имя шаблона
+        model.addAttribute("orders", orders);
         return "orders";
     }
 
-    @GetMapping("/new_order")
-    public String showNewOrderPage() {
-        return "new_order";
+    @PostMapping("/status/{id}")
+    public String updateStatus(@PathVariable Long id, @RequestParam("status") String status) {
+        OrderStatus newStatus = OrderStatus.valueOf(status);
+        orderService.setStatus(id, newStatus);
+        return "redirect:/orders";
     }
-
 }
--- src/main/java/com/springboot/printmastercrm/service/OrderService.java
+++ src/main/java/com/springboot/printmastercrm/service/OrderService.java
@@ -1,18 +1,34 @@
 package com.springboot.printmastercrm.service;
 
 import com.springboot.printmastercrm.entity.Order;
+import com.springboot.printmastercrm.entity.OrderStatus;
 import com.springboot.printmastercrm.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class OrderService {
+
     @Autowired
     private OrderRepository orderRepository;
 
-
-    public void saveOrder(Order order) {
-        orderRepository.save(order);
+    public void setStatus(Long orderId, OrderStatus status) {
+        Optional<Order> optional = orderRepository.findById(orderId);
+        if (optional.isPresent()) {
+            Order order = optional.get();
+            order.setStatus(status);
+            orderRepository.save(order);
+        }
     }
 
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public List<Order> getOrdersByStatus(OrderStatus status) {
+        return orderRepository.findByStatus(status);
+    }
 }
